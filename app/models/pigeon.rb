ebc4b6ee8ce07c593ebebbce1528ee01825d1e11
thais paris <thais.paris.mallez@gmail.com>
2021-11-16 10:29:16 +0100

    variable breeds mit au pluriel + migration droppigeon supprimé

--- app/models/pigeon.rb
+++ app/models/pigeon.rb
@@ -1,9 +1,9 @@
 class Pigeon < ApplicationRecord
-  BREED = ['ramier', 'biset', 'vert', 'victoria', 'frisé', 'bleu couronné', 'ailes de bronze', 'jacobin', 'indien', 'vert africain', 'plumifère', 'nicobar' ]
+  BREEDS = ['ramier', 'biset', 'vert', 'victoria', 'frisé', 'bleu couronné', 'ailes de bronze', 'jacobin', 'indien', 'vert africain', 'plumifère', 'nicobar' ]
 
   validates :name, presence: true
   validates :adress, presence: true
   validates :description, presence: true
   validates :price_day, presence: true
-  validates :breed, inclusion: { in: BREED }
+  validates :breed, inclusion: { in: BREEDS }
 end
