dacd63361455180c1c0cff583b16c8d6b52fc919
thais paris <thais.paris.mallez@gmail.com>
2021-11-16 11:58:27 +0100

    correction allow blank

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,7 +2,7 @@ class Booking < ApplicationRecord
 
   validates :adress, presence: true
   validates :date, presence: true
-  validates :message, presence: true, allow_blank: false
+  validates :message, presence: true
   validates :recipient_name, presence: true
   validates :option_droppings, presence: true, :default => false
   validates :option_cooing, presence: true, :default => false
