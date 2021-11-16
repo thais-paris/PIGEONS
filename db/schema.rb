1273e573f9de8b8dea8b6c88e38d9d5356d15422
Gregoire Vallette <gregoire.vallette@gmail.com>
2021-11-16 12:25:33 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_15_173001) do
+ActiveRecord::Schema.define(version: 2021_11_16_104135) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,21 @@ ActiveRecord::Schema.define(version: 2021_11_15_173001) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
+  create_table "bookings", force: :cascade do |t|
+    t.string "adress"
+    t.date "date"
+    t.text "message"
+    t.string "recipient_name"
+    t.boolean "option_droppings"
+    t.boolean "option_cooing"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.bigint "pigeon_id", null: false
+    t.index ["pigeon_id"], name: "index_bookings_on_pigeon_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
   create_table "pigeons", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -44,6 +59,8 @@ ActiveRecord::Schema.define(version: 2021_11_15_173001) do
     t.float "price_day"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_pigeons_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -63,4 +80,7 @@ ActiveRecord::Schema.define(version: 2021_11_15_173001) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "bookings", "pigeons"
+  add_foreign_key "bookings", "users"
+  add_foreign_key "pigeons", "users"
 end
