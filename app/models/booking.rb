df9047f208e4606b5ae188ce214c718dc0654bde
thais paris <thais.paris.mallez@gmail.com>
2021-11-16 11:09:24 +0100

    création du model booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,8 @@
 class Booking < ApplicationRecord
+  validates :adress, presence: true
+  validates :date, presence: true
+  validates :message, presence: true, allow_blank: false
+  validates :recipient_name, presence: true
+  validates :option_droppings, presence: true, :default => false
+  validates :option_cooing, presence: true, :default => false
 end
