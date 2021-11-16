34a5a413158b331b42c903fc220bd395a99edc89
Flavia Gallois <fgbis@hotmail.com>
2021-11-16 23:28:36 +0100

    controller views booking

---
+++ app/controllers/bookings_controller.rb
@@ -0,0 +1,53 @@
+class BookingsController < ApplicationController
+  def index
+    @bookings = policy_scope(Booking).order(created_at: :desc)
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+    authorize @booking
+    @pigeon = params[:pigeon]
+    @address = params[:address]
+    @date = params[:date]
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    authorize @booking
+    @booking.pigeon = Pigeon.find(params[:pigeon_id])
+    @booking.user = current_user
+    @booking.date = params[:date]
+    @booking.address = params[:address]
+    @booking.save
+    redirect_to booking_path(@booking)
+  end
+
+  def edit
+    @booking = Booking.find(params[:id])
+    authorize @booking
+  end
+
+  def update
+    @booking = Booking.find(params[:id])
+    authorize @booking
+    @booking.update(booking_params)
+    redirect_to booking_path(@booking)
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    authorize @booking
+    @booking.destroy
+    redirect_to bookings_path
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:message, :recipient_name, :option_droppings, :option_cooing)
+  end
+end
---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,31 @@
+class BookingPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    record.user == user
+  end
+
+  def new?
+    create?
+  end
+
+  def create?
+    true
+  end
+
+  def edit?
+    update?
+  end
+
+  def destroy?
+    update?
+  end
+
+  def update?
+    record.user == user
+  end
+end
