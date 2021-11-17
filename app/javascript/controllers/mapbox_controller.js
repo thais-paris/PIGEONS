035e7de620b0641da1241bfe55f8e2a6a69e192a
Flavia Gallois <fgbis@hotmail.com>
2021-11-17 16:09:28 +0100

    set up map geo dans search

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -13,23 +13,55 @@ export default class extends Controller {
     this.map = new mapboxgl.Map({
       container: this.element,
       style: 'mapbox://styles/mapbox/streets-v10'
+      // 'mapbox://styles/fgbis/ckw3lkntxbu6c14pl827s3bqx'
     });
     this._addMarkersToMap();
     this._fitMapToMarkers();
+    this.map.resize();
   }
 
-  _addMarkersToMap() {
-    this.markersValue.forEach((marker) => {
-      new mapboxgl.Marker()
-        .setLngLat([marker.lng, marker.lat])
-        .addTo(this.map);
-    });
-  }
+  // _addMarkersToMap() {
+  //   this.markersValue.forEach((marker) => {
+  //     new mapboxgl.Marker()
+  //       .setLngLat([marker.lng, marker.lat])
+  //       .addTo(this.map);
+  //   });
+  // }
 
   _fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds();
     this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]));
-    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 1000 });
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 300 });
+  }
+
+  // _addMarkersToMap() {
+  //   this.markersValue.forEach((marker) => {
+  //     const popup = new mapboxgl.Popup().setHTML(marker.info_window); // add this
+  //     new mapboxgl.Marker()
+  //       .setLngLat([marker.lng, marker.lat])
+  //       .setPopup(popup) // add this
+  //       .addTo(this.map);
+  //   });
+  // }
+
+  _addMarkersToMap() {
+    this.markersValue.forEach((marker) => {
+      const popup = new mapboxgl.Popup().setHTML(marker.info_window);
+
+      // Create a HTML element for your custom marker
+      const customMarker = document.createElement('div');
+      customMarker.className = 'marker';
+      customMarker.style.backgroundImage = `url('${marker.image_url}')`;
+      customMarker.style.backgroundSize = 'contain';
+      customMarker.style.width = '25px';
+      customMarker.style.height = '25px';
+
+      // Pass the element as an argument to the new marker
+      new mapboxgl.Marker(customMarker)
+        .setLngLat([marker.lng, marker.lat])
+        .setPopup(popup)
+        .addTo(this.map);
+    });
   }
 
 }
